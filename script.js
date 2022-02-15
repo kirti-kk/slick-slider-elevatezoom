9542d3c60245e09937a707b23b1d3c0f3f78886b
kirti-kk <99725736+kirti-kk@users.noreply.github.com>
2022-02-15 15:44:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css"/>
+<script src="https://codepen.io/manikoth/pen/xWKOda.js"></script>
